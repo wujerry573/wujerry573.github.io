1a621b07246fa26def32ad258eefb8374a12a324
Jerry Wu <wujerry573@gmail.com>
2020-03-10 21:44:17 -0400

    Updates

---
+++ precache-manifest.075fecdd0f31e899975960801126c220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b2f4829104e6a1490b592b5a0bff94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ad6b58ed2872afba52a",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "f3c815d065428dd7b8e4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3ad6b58ed2872afba52a",
+    "url": "/static/js/2.3400cdab.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3400cdab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c815d065428dd7b8e4",
+    "url": "/static/js/main.9c7ac0c2.chunk.js"
+  },
+  {
+    "revision": "52b86d121845e2bb9985",
+    "url": "/static/js/runtime-main.ff28a923.js"
+  }
+]);
